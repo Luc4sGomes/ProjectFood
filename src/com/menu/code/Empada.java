f38cfe8a5ab0d264c49bd0ef8a0c045924ce510e
lucas <lucasgominhaa448@gmail.com>
2021-02-07 00:40:35 -0300

    added empada

---
+++ src/com/menu/code/Empada.java
@@ -0,0 +1,35 @@
+package com.menu.code;
+
+public class Empada {
+
+    private int[] quant = new int [100];
+
+    private int kind;
+
+    private double valor = 2.50;
+
+
+    public int[] getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int[] quant) {
+        this.quant = quant;
+    }
+
+    public int getKind() {
+        return kind;
+    }
+
+    public void setKind(int kind) {
+        this.kind = kind;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
